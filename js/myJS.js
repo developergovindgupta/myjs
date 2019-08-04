a049ace7eeaded629490fbe124729d2ef260ae33
Govind Prasad Gupta <govindprasadguptamca@gmail.com>
2019-08-04 18:30:08 +0530

    Add tow methods load and ready with their respective html manual and also modify index.html give download link of myJS.js file

--- js/myJS.js
+++ js/myJS.js
@@ -1,4 +1,48 @@
 (function () {
+    document.ready = function (fn) {
+        if (typeof fn === 'function') {
+            document.addEventListener('readystatechange', function () {
+                if (document.readyState === 'complete') {
+                    fn();
+                }
+            });
+        }
+
+    };
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                                                        //
+    // getElementById, getElementByTagName, querySelector  Extension Methods                  //
+    //                                                                                        //
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    let node = document.createElement("input");
+    /*
+    ======================================================================================
+    It load passed page url content in target DOM element asynchronously. 
+    It follows Promise structure.
+    ======================================================================================
+    */
+    node.__proto__.__proto__.load = function (url) {
+        let container = this;
+        let p = new Promise(function (resolve, reject) {
+            try {
+                let xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function (e) {
+                    if (xhr.readyState === 4 && xhr.status === 200) {
+                        container.innerHTML = xhr.responseText;
+                        resolve(container);
+                    } else if (xhr.readyState === 4) {
+                        reject(xhr);
+                    }
+                };
+                xhr.open("GET", url, true);
+                xhr.setRequestHeader("Content-type", "text/html");
+                xhr.send();
+            } catch (ex) {
+                reject(ex);
+            }
+        });
+        return p;
+    };
 
 
 })();
